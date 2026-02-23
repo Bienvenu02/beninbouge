7faf4bff803e11986d976be66b8886f32767d71d
Bienvenu02 <bienvenuhounye05@gmail.com>
2026-02-23 02:17:21 +0100

    add

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ php artisan storage:link
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-exec frankenphp run
+exec frankenphp run --config /etc/caddy/Caddyfile --adapter caddyfile
 ```
 
 Et crée un fichier `Caddyfile` à la racine de ton projet :
