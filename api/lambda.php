fd52b53e34bcc406b0c0e28a06a3b19c74c3d5e2
Bienvenu02 <bienvenuhounye05@gmail.com>
2026-02-23 21:03:04 +0100

    add

--- api/lambda.php
+++ api/lambda.php
@@ -5,19 +5,26 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+// Déterminer le chemin de base
+$basePath = $_SERVER['APP_BASE_PATH'] ?? dirname(__DIR__);
+
+// Vérifier si l'application est en mode maintenance
+if (file_exists($maintenance = $basePath . '/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Remplace les lignes de chemins par :
-$basePath = $_SERVER['APP_BASE_PATH'] ?? dirname(__DIR__);
-
-// Register the Composer autoloader...
+// Charger l'autoloader
 require $basePath . '/vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
+// Bootstrap Laravel
 /** @var Application $app */
 $app = require_once $basePath . '/bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+// Gérer la requête
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+$response->send();
+
+$kernel->terminate($request, $response);
