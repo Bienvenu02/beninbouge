f5f64942b56cfd792e7d1ab7af8a99ef14311e5e
Bienvenu02 <bienvenuhounye05@gmail.com>
2026-02-24 16:40:52 +0100

    add

--- api/index.php
+++ api/index.php
@@ -1,6 +1,23 @@
 <?php
 
-// Définir le bon répertoire racine
-$_SERVER['APP_BASE_PATH'] = dirname(__DIR__);
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
-require dirname(__DIR__) . '/public/index.php';
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Remplace les lignes de chemins par :
+$basePath = $_SERVER['APP_BASE_PATH'] ?? dirname(__DIR__);
+
+// Register the Composer autoloader...
+require $basePath . '/vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once $basePath . '/bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
